1d16e0831b7566f71d392d669d840de908f384f5
Victor Fernandes Neto <vvfernandesneto@gmail.com>
2023-10-06 12:00:52 -0300

    commit pra deploy, a desenvolver versionamento

--- apps/framedata/views.py
+++ apps/framedata/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
 from apps.framedata.models import Character, Move
 from apps.framedata.serializer import CharacterSerializer, MoveSerializer, ListMoveCharacterSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CharacterViewSet(viewsets.ModelViewSet):
     """showing all characters"""
@@ -15,6 +16,9 @@ class MoveViewSet(viewsets.ModelViewSet):
     """showing all moves"""
     queryset = Move.objects.all()
     serializer_class = MoveSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['move_name']
+    filterset_fields = ['character']
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -24,5 +28,7 @@ class ListCharacterMove(generics.ListAPIView):
         queryset = Move.objects.filter(character_id = self.kwargs['pk'])
         return queryset
     serializer_class = ListMoveCharacterSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['move_name']
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
